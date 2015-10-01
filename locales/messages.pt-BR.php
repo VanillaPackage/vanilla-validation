2e754e1a4a2ff66aa0988a104bfe027fde6dd46b
David Rodrigues <david.proweb@gmail.com>
2015-10-01 16:42:36 -0300

    Update pt-BR localization;

--- locales/messages.pt-BR.php
+++ locales/messages.pt-BR.php
@@ -2,34 +2,48 @@
 
 return [
     'fail:array' => 'o campo ":field" deve ser um array',
-    'fail:notArray' => 'o campo ":field" não pode ser um array',
+    'fail:notArray' => 'o campo ":field" não deve conter um array',
+
     'fail:boolean' => 'o campo ":field" deve ser um booleano',
-    'fail:notBoolean' => 'o campo ":field" não pode ser um booleano',
-    'fail:cep' => 'o campo ":field" deve ser um CEP',
-    'fail:notCep' => 'o campo ":field" não pode ser um CEP',
-    'fail:cnpj' => 'o campo ":field" deve ser um CNPJ',
-    'fail:notCnpj' => 'o campo ":field" não pode ser um CNPJ',
-    'fail:cpf' => 'o campo ":field" deve ser um CPF',
-    'fail:notCpf' => 'o campo ":field" não pode ser um CPF',
-    'fail:date' => 'o campo ":field" deve ser uma data',
-    'fail:notDate' => 'o campo ":field" não pode ser uma data',
-    'fail:email' => 'o campo ":field" deve ser um e-mail',
-    'fail:notEmail' => 'o campo ":field" não pode ser um e-mail',
+    'fail:notBoolean' => 'o campo ":field" não deve conter um booleano',
+
+    'fail:cep' => 'o campo ":field" deve ser um CEP válido',
+    'fail:notCep' => 'o campo ":field" não deve conter um CEP',
+
+    'fail:cnpj' => 'o campo ":field" deve ser um CNPJ válido',
+    'fail:notCnpj' => 'o campo ":field" não deve conter um CNPJ',
+
+    'fail:cpf' => 'o campo ":field" deve ser um CPF válido',
+    'fail:notCpf' => 'o campo ":field" não deve conter um CPF',
+
+    'fail:date' => 'o campo ":field" deve ser uma data válida',
+    'fail:notDate' => 'o campo ":field" não deve conter uma data',
+
+    'fail:email' => 'o campo ":field" deve ser um e-mail válido',
+    'fail:notEmail' => 'o campo ":field" não deve conter um e-mail',
+
     'fail:empty' => 'o campo ":field" deve estar vazio',
-    'fail:notEmpty' => 'o campo ":field" não pode estar vazio',
+    'fail:notEmpty' => 'o campo ":field" não deve estar vazio',
+
     'fail:equals' => 'o campo ":field" não possui o valor esperado',
+
     'fail:mask' => 'o campo ":field" deve ser definido no formato especificado',
     'fail:notMask' => 'o campo ":field" não pode ser definido no formato especificado',
+
     'fail:maxLength' =>
         '{1} o campo ":field" deve possuir no máximo um caractere|' .
         'o campo ":field" deve possuir no máximo :length caracteres',
+
     'fail:minLength' =>
         '{1} o campo ":field" deve possuir no mínimo um caractere|' .
         'o campo ":field" deve possuir no mínimo :length caracteres',
+
     'fail:required' => 'o campo ":field" é obrigatório',
+
     'fail:sameLength' =>
         '{1} o campo ":field" deve possuir exatamente um caractere|' .
         'o campo ":field" deve possuir exatamente :length caracteres',
+
     'fail:string' => 'o campo ":field" deve ser uma string',
-    'fail:notString' => 'o campo ":field" não pode ser uma string',
+    'fail:notString' => 'o campo ":field" não deve ser uma string',
 ];
